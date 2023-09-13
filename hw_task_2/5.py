7bf3e7e84eee10e087b9d22295b8f8bac3332485
versh <vershinindv42@gmail.com>
2023-09-14 01:24:13 +0300

    hw2 task 5

--- hw_task_2/5.py
+++ hw_task_2/5.py
@@ -1,2 +1,14 @@
 # Stairs
 
+def countStairs(lower, n):
+    if n == 0:
+        return 1
+    count = 0
+    for i in range(lower):
+        if n - i < 0:
+            break
+        count += countStairs(i, n-i)
+    return count
+
+N = 7
+print(countStairs(N+1,N))
